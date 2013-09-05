63b61667d1fd9f13ad3a949478128e1eee36e837
mmasilon <astronut98@hotmail.com>
2013-09-04 23:22:41 -0700

    Added Course Catalog option block
    Added Course Catalog option block to read the data from the courseList.txt file, parse, sort and call toStringCourse() method from Course class to display course catalog to screen.

--- x464010/teamb/srs/StudentRegistrationSystem.java
+++ x464010/teamb/srs/StudentRegistrationSystem.java
@@ -1,61 +1,66 @@
-package x464010.teamb.srs;
-
+package x46010.teamb.srs;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Collections;
+import java.io.File;
+import java.io.*;
+import java.util.*;
 
 /**
  * WelcomeMenu class starts the SRS application
  *
  * @author Amit Dhamija
- * @version 1.1
+ * @version 1.2
+ * @revision 1.1 Amit Dhamija 		Whitespace adjustments
+ * @revision 1.2 Michelle Masilon 	Added course catalog option
  */
 public class StudentRegistrationSystem {
 
 	private static final int COURSE_CATALOG 		= 1;
 	private static final int STUDENT_ACCOUNT_LOGIN 	= 2;
 	private static final int NEW_STUDENT_ACCOUNT 	= 3;
-	
+
 	public static final String STARS = "********************";
 	public static final String SELECT_OPTION = "Select an option, then press ENTER: ";
-	
+
 	private static LoginView login;
 	/**
-	 * 
+	 *
 	 */
 	public StudentRegistrationSystem() {
-		
+
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		login = new LoginView();
 		Scanner userInputScanner;
-		
+
 		try {
 			// set up Scanner object using system.in
 			userInputScanner = new Scanner(System.in);
-			
+
 			// display menu options
 			showMainMenu();
 
 			// retrieve response
 			int option = Integer.parseInt(userInputScanner.next());
 			onOptionSelect(option);
-			
+
 			userInputScanner.close();
 		} catch (InputMismatchException e) {
 			System.out.println("Error! Incorrect input type: " + e.getMessage());
 		}
 	}
-	
+
 	public static void showMainMenu() {
-		
-		String srsTitle = " Student Registration System ";
-		
+
+		String srsTitle = "Student Registration System ";
+
 		System.out.println(STARS + srsTitle + STARS);
 		System.out.println();
 		System.out.println("1. Course Catalog");
@@ -64,16 +69,37 @@ public class StudentRegistrationSystem {
 		System.out.println();
 		System.out.print(SELECT_OPTION);
 	}
-	
+
 	public static void onOptionSelect(int option) {
 		switch (option) {
-			case COURSE_CATALOG:	
-				//courseCatalog.showList();
+			case COURSE_CATALOG:
+				try {
+					File courseListFile = new File("CourseList.txt");
+					Scanner fileScanner = new Scanner(courseListFile);
+					ArrayList<Course> courseList = new ArrayList<Course>();
+					Course tempCourse;
+
+					while (fileScanner.hasNextLine()) {
+						String[] courseAttributes = fileScanner.nextLine().split(",");
+						tempCourse = new Course(courseAttributes[0], courseAttributes[1], courseAttributes[2], courseAttributes[3],courseAttributes[4],new Integer(courseAttributes[5]).intValue(),new Integer(courseAttributes[6]).intValue());
+						courseList.add(tempCourse);
+					}
+						Collections.sort(courseList);
+					for (Course course : courseList) {
+						System.out.println(course.toStringCourse());
+					}
+						fileScanner.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
 				break;
-			case STUDENT_ACCOUNT_LOGIN:	
+			case STUDENT_ACCOUNT_LOGIN:
 				login.getInput();
 				break;
-			case NEW_STUDENT_ACCOUNT:	
+			case NEW_STUDENT_ACCOUNT:
 				//studentAccount.getInput();
 				break;
 			default:
@@ -81,4 +107,5 @@ public class StudentRegistrationSystem {
 				break;
 		}
 	}
+
 }
