b42e3ea0cb4b1f3463b5108184cb7168d92879fa
mmasilon <astronut98@hotmail.com>
2013-09-13 14:53:35 -0700

    Added check to keep full classes from being displayed in course catalog

--- source/x464010/teamb/srs/CourseCatalog.java
+++ source/x464010/teamb/srs/CourseCatalog.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
  * 
  * @author Amit Dhamija
  * @version 1.4
- * @revision 1.1	Amit Dhamija	Added showCourseList method; 
- * 									moved Michelle's code from StudentRegistrationSystem to this method
- * @revision 1.2	Amit Dhamija	Updated the class to use modified Console class methods
- * @revision 1.3	Amit Dhamija	Added showCatalog() method; added list of options to show
- * @revision 1.4	Amit Dhamija	Added options to register/unregister course
+ * @revision 1.1	Amit Dhamija		Added showCourseList method; moved Michelle's code from StudentRegistrationSystem to this method
+ * @revision 1.2	Amit Dhamija		Updated the class to use modified Console class methods
+ * @revision 1.3	Amit Dhamija		Added showCatalog() method; added list of options to show
+ * @revision 1.4	Amit Dhamija		Added options to register/unregister course
+ * @revision 1.5	Michelle Masilon	Added check to keep full classes from being displayed in course catalog
  */
 public class CourseCatalog extends Console {
 	
@@ -87,7 +87,12 @@ public class CourseCatalog extends Console {
 				String[] courseAttributes = fileScanner.nextLine().split(",");
 				tempCourse = new Course(courseAttributes[0], courseAttributes[1], courseAttributes[2], courseAttributes[3],courseAttributes[4],new Integer(courseAttributes[5]).intValue(),new Integer(courseAttributes[6]).intValue());
 				courseList.add(tempCourse);
+				
+				if(new Integer(courseAttributes[5]).intValue() != new Integer(courseAttributes[6]).intValue()) {
+					courseList.add(tempCourse);
+					}
 			}
+			
 			
 			Collections.sort(courseList);
 			for (Course course : courseList) {
